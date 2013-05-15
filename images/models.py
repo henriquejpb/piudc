78e1ed789c8e4f0f72eb1e0113a9d8ea3cdf5754
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-05-15 15:27:06 +0200

    Fixa un bug con el renombremiento de imágenes

--- images/models.py
+++ images/models.py
@@ -16,24 +16,45 @@ class Image(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        if self.id == None:
+            fn, ext = os.path.splitext(self.pathname.name)
+            new_filename = os.path.join(UPLOAD_DIR, str(uuid.uuid4()) + ext)
+            self.pathname.name = new_filename
+
+        super(Image, self).save(*args, **kwargs)
+
+        filepath = os.path.join(settings.BASE_DIR, self.pathname.url[1:])
+        self.correct_dimensions(filepath)
+
+    def correct_dimensions(self, filepath):
+        image = PIL.Image.open(filepath)
+    
+        width, height = image.size
+        if width > MAX_WIDTH or height > MAX_HEIGHT:
+            self.resize(image, width, height)
+        image.save(filepath)
+
+    def resize(self, image, width, height):
+        factor = min(MAX_WIDTH/width, MAX_HEIGHT/height)
+        new_width = int(round(factor * width))
+        new_height = int(round(factor * height))
+
+        image.thumbnail((new_width, new_height), PIL.Image.ANTIALIAS)
+
+    def save_remote(self, *args, **kwargs):
         fn, ext = os.path.splitext(self.pathname.name)
-        new_filename = str(uuid.uuid4()) + ext
+        new_filename = os.path.join(UPLOAD_DIR, str(uuid.uuid4()) + ext)
+        
+        temp_absolute_path = os.path.join(settings.MEDIA_ROOT, self.pathname.name)
+        new_absolute_path = os.path.join(settings.MEDIA_ROOT, new_filename)
+
+        os.rename(temp_absolute_path, new_absolute_path)
         self.pathname.name = new_filename
 
         super(Image, self).save(*args, **kwargs)
 
-        filename = os.path.join(settings.BASE_DIR, self.pathname.url[1:])
-        image = PIL.Image.open(filename)
-    
-        width, height = image.size
-        if width > MAX_WIDTH or height > MAX_HEIGHT:
-            factor = min(MAX_WIDTH/width, MAX_HEIGHT/height)
-            print factor
-            new_width = int(round(factor * width))
-            new_height = int(round(factor * height))
-        
-            image.thumbnail((new_width, new_height), PIL.Image.ANTIALIAS)
-            image.save(filename)
+        filepath = os.path.join(settings.BASE_DIR, self.pathname.url[1:])
+        self.correct_dimensions(filepath)
 
     def delete(self, *args, **kwargs):
         super(Image, self).delete(*args, **kwargs)
